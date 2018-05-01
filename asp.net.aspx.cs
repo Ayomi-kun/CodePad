ce45360f527fdea85c1736985d77eaa2a349e0c4
Ayomikun Oluwayemi <oluwayemiayo@yahoo.com>
2018-05-01 08:13:28 +0100

    console.write for web

--- asp.net.aspx.cs
+++ asp.net.aspx.cs
@@ -4,9 +4,11 @@
         // this executes the code here when the form is submitted not when the form is first presented to the user
         // isPostBack will be false when the page is first presented but true when the page is submitted again with the post method
     }
-    // another way to do this is to add a button click event "OnClick" to the button by referencing it to the button ID.
-    OnClick = "buttonID_Click"
+    // another way to do this is to add a button click event "OnClick" to the button by referencing it to the button ID and this creates an event in the .cs file(back end).
+    OnClick = "buttonID_Click";
 
 // having a literal is cool just like a label anf you can change its text after you have performed some functions on the page.
-    literalID.Text = "Something new"
+    literalID.Text = "Something new";
 
+//just like Console.write for cli c# applications, this is for web application
+    Responce.Write("Hello from webforms application");
